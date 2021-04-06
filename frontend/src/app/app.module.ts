b4979f514fba2805b02adf12fefc221bce1a02ee
satya9500 <satya.prakash9500@gmail.com>
2021-04-07 00:06:57 +0530

    stable nebular

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VoicelyComponent } from './voicely/voicely.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import {
+  NbActionsModule, NbButtonModule, NbCardModule, NbContextMenuModule,
+  NbIconModule, NbInputModule,
+  NbLayoutModule, NbListModule, NbMenuModule, NbMenuService, NbSearchModule,
+  NbSidebarModule,
+  NbSidebarService, NbSpinnerModule, NbStepperModule,
+  NbThemeModule, NbToastrModule,
+  NbUserModule
+} from '@nebular/theme';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 
@@ -16,7 +28,33 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbSidebarModule, // NbSidebarModule.forRoot(), //if this is your app.module
+    NbButtonModule,
+    NbSidebarModule.forRoot(),
+    NbThemeModule.forRoot(),
+    NbEvaIconsModule,
+    NbIconModule,
+    NbLayoutModule,
+    NbActionsModule,
+    NbSidebarModule,
+    NbUserModule,
+    NbMenuModule.forRoot(),
+    NbCardModule,
+    HttpClientModule,
+    NbContextMenuModule,
+    NbStepperModule,
+    NbButtonModule,
+    NbInputModule,
+    ReactiveFormsModule,
+    NbSpinnerModule,
+    NbToastrModule.forRoot(),
+    NbSearchModule,
+    NbListModule,
   ],
   providers: [HttpClient],
   bootstrap: [AppComponent]
