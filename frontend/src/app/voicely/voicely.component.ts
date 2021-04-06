18056caa099cd4181984582cfd8c1d2ae7b2199a
satya9500 <satya.prakash9500@gmail.com>
2021-04-07 00:06:57 +0530

    stable nebular

--- frontend/src/app/voicely/voicely.component.ts
+++ frontend/src/app/voicely/voicely.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FetchService} from '../services/fetch.service'
+import { NbMenuItem, NbSidebarService } from '@nebular/theme';
 
 @Component({
   selector: 'app-voicely',
@@ -8,12 +9,52 @@ import {FetchService} from '../services/fetch.service'
 })
 export class VoicelyComponent implements OnInit {
 
-  constructor(private fetchService: FetchService) { }
+  constructor(private fetchService: FetchService, private sidebarService: NbSidebarService) { }
+
+  public user = {name: "Satya Prakash"};
 
   ngOnInit(): void {
     this.fetchService.checkAPIServices().subscribe((res:any) => {
       console.log(res);
     })
+  }
+
+  public items: NbMenuItem[] = [
+    {
+      title: 'Home',
+      icon: 'home-outline',
+      link: '/app/home'
+    },
+    {
+      title: 'Clone Voice',
+      icon: 'headphones-outline',
+      link: '/app/recycle'
+    },
+    {
+      title: 'Let\'s Voicely',
+      icon: 'checkmark-square-outline',
+      link: '/app/history'
+    },
+    {
+      title: 'Statistics',
+      icon: 'activity-outline',
+      link: '/app/report'
+    },
+    {
+      title: 'My History',
+      icon: 'archive-outline',
+      link: '/app/rewards'
+    },
+    {
+      title: 'Logout',
+      icon: 'unlock-outline',
+      link: '/auth/logout'
+    },
+  ];
+
+  toggle() {
+    this.sidebarService.toggle(true);
+    return false;
   }
 
 }
