fe562a25dc65651e2b58db88368dc3a762697dc4
lite <chauhanharsh3363334@gmail.com>
2021-04-07 15:32:37 +0530

    done

--- ML/voice_cloning/main.py
+++ ML/voice_cloning/main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from demo_cli import *
 
+
 import librosa
 import numpy as np
 import soundfile as sf
@@ -27,6 +28,8 @@ async def noise_rm():
     noise_reduced = nr.reduce_noise(audio_clip=original_wav, noise_clip=noise_clip, verbose=False)
     sf.write(output_path, noise_reduced.astype(np.float64), sampling_rate)
     return {"Message": "Status 200 application is ok"}
+
+@app.post("newReq")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
