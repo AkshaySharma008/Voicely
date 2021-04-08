56da6cc76a5eb30e284d3f2fd860588a9323ce5f
AkshaySharma008 <akshaysharma30121999@gmail.com>
2021-04-08 12:30:59 +0530

    UI Changes

--- frontend/src/app/voicely/lets-voicely/lets-voicely.component.ts
+++ frontend/src/app/voicely/lets-voicely/lets-voicely.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgAudioRecorderService, OutputFormat } from 'ng-audio-recorder';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-lets-voicely',
@@ -7,9 +11,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LetsVoicelyComponent implements OnInit {
 
-  constructor() { }
+  constructor( private audioRecorderService: NgAudioRecorderService ,private formBuilder: FormBuilder) { 
+    this.audioRecorderService.recorderError.subscribe(recorderErrorCase => {
+      // Handle Error
+  })
+  }
+
+  public blobUrl;
+  audioUploadForm:FormGroup;
 
   ngOnInit(): void {
+    this.audioUploadForm = this.formBuilder.group({
+      audio:['', Validators.required]
+    })
   }
+
+  startRecording() {
+    this.audioRecorderService.startRecording();
+  }
+
+  stopRecording() {
+     this.audioRecorderService.stopRecording(OutputFormat.WEBM_BLOB_URL).then((output) => {
+        // do post output steps
+        this.blobUrl = output;
+        console.log(this.blobUrl);
+        // this.sendAudioFile(this.blobUrl);
+
+     }).catch(errrorCase => {
+         // Handle Error
+         console.log('error aae');
+         console.log(errrorCase)
+     });
+  }
+
+  onUpload(e){
+    console.log(e);
+  }
+
+  // sendAudioFile = file => {
+  //   const formData = new FormData();
+  //   formData.append('audio-file', file);
+  //   return fetch(this.blobUrl, {
+  //     method: 'POST',
+  //     body: formData
+  //   });
+  // };
+
 
 }
