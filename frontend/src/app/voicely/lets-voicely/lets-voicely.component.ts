58f102e532a040be33549a26c5851f5504123643
satya9500 <satya.prakash9500@gmail.com>
2021-04-09 16:16:32 +0530

    stepper done

--- frontend/src/app/voicely/lets-voicely/lets-voicely.component.ts
+++ frontend/src/app/voicely/lets-voicely/lets-voicely.component.ts
@@ -17,14 +17,22 @@ export class LetsVoicelyComponent implements OnInit {
   }
 
   public blobUrl;
-  audioUploadForm:FormGroup;
+  public audioUploadForm:FormGroup;
+  public correctedTextForm: FormGroup;
   public convertedText;
   public audio_path;
+  public showOverlay = false;
+  public s3Link='https://arkynexbucket.s3.amazonaws.com/output_1617964161747494.wav';
 
   ngOnInit(): void {
     this.audioUploadForm = this.formBuilder.group({
       audio:['', Validators.required]
     })
+    this.correctedTextForm = this.formBuilder.group({
+      correctedText:['', Validators.required],
+      inputText: ['', Validators.required]
+    })
+
   }
 
   public audio_file;
@@ -36,17 +44,22 @@ export class LetsVoicelyComponent implements OnInit {
   sendAudioFile() {
     const formData = new FormData;
     formData.append('audio', this.audio_file);
+    this.showOverlay = true;
     this.addService.saveAudio(formData).subscribe((res:any) => {
       console.log(res);
-      this.convertedText = res.data;
+      this.correctedTextForm.patchValue({inputText: res.input_speech})
+      this.correctedTextForm.patchValue({correctedText: res.data})
       this.audio_path = res.audio_file_name;
+      this.showOverlay = false;
     })
   };
 
   cloneAudio() {
-    this.addService.cloneAudio({path: this.audio_path, message: this.convertedText}).subscribe((res:any)=>{
+    this.showOverlay = true;
+    this.addService.cloneAudio({path: this.audio_path, message: this.correctedTextForm.value.correctedText}).subscribe((res:any)=>{
       console.log(res);
+      this.showOverlay = false;
+      this.s3Link = res.s3Data.Location;
     })
   }
-
 }
